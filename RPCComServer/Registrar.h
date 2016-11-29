759bcb4c4a266b782bc2aee8833c78c06c45e24e
abhishekp314 <abhishek.p.3.14@gmail.com>
2016-11-29 10:50:32 +0530

    License Headers updated.a

--- RPCComServer/Registrar.h
+++ RPCComServer/Registrar.h
@@ -1,3 +1,9 @@
+/* XCFrameworkEngine
+ * Copyright (C) Abhishek Porwal, 2016
+ * Any queries? Contact author <https://github.com/abhishekp314>
+ * This program is complaint with GNU General Public License, version 3.
+ * For complete license, read License.txt in source root directory. */
+
 
 #if !defined(_REGISTRAR_H)
 #define _REGISTRAR_H
--- RPCComServer/stdafx.cpp
+++ RPCComServer/stdafx.cpp
@@ -1,3 +1,9 @@
+/* XCFrameworkEngine
+ * Copyright (C) Abhishek Porwal, 2016
+ * Any queries? Contact author <https://github.com/abhishekp314>
+ * This program is complaint with GNU General Public License, version 3.
+ * For complete license, read License.txt in source root directory. */
+
 // stdafx.cpp : source file that includes just the standard includes
 // RPCComServer.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
